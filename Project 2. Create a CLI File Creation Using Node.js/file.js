e52c8398e8167089a7a01f7d46f243613d547d53
Shah Wali <69451223+shahwali-web@users.noreply.github.com>
2025-04-15 12:57:39 +0500

    Update file.js

--- Project 2. Create a CLI File Creation Using Node.js/file.js
+++ Project 2. Create a CLI File Creation Using Node.js/file.js
@@ -1,13 +1,4 @@
-// const fs = require("fs")
 
-// fs.writeFileSync("test.txt","this is text Updated", 'utf-8')
-// fs.appendFileSync("test.txt","\nThis text come from Append")
-// const a = fs.readFileSync("test.txt","utf-8")
-
-// for(let i =0; i <5; i++){
-    
-//     fs.appendFileSync("test.txt","\nThis is Append In side for Loop ")
-// }
 import readline from 'readline'
 import fs from 'fs'
 
@@ -17,12 +8,7 @@ const r1 = readline.createInterface({
     output: process.stdout
 })
 
-// r1.question("Enter your name ", function(name){
-//     console.log("Your name is : ", name)
-//     const final_value = `My Name is ${name}`
-//     fs.appendFileSync("test.txt", `\n${final_value}`)
-//     r1.close();
-// })
+
 
 r1.question("Enter Your File name: ", function(fileName){
     r1.question("Enter Your Text here", function(userText){
@@ -37,6 +23,3 @@ r1.question("Enter Your File name: ", function(fileName){
     })
    
 })
-// user_text = question("Enter Your Text here: ", function(userText){
-//     fs.writeFileSync("")
-// })
